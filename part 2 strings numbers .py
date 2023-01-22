a0c9213fda9ea135c77d751f57bf8ec6355eeaa9
Daud Khan <109561961+daudkhan2024@users.noreply.github.com>
2023-01-22 13:49:10 +0100

    Add files via upload
    Here you will find the most common and basic function of strings which I used in Data Science

---
+++ part 2 strings numbers .py
@@ -0,0 +1,46 @@
+# Strings
+# anyting between quotations "" are called strings
+
+
+print("muhammad khan")
+# we can also do in a way
+
+phrase = "muhammad khan"
+print(phrase +" is cool")
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.isalnum())
+print(phrase.upper().isupper())
+print(len(phrase))
+
+
+
+#lets see whats first character
+print(phrase[0])#it will show at 0 which element is present
+print(phrase[7])
+print(phrase.index("a"))#it will show use the exact poition
+print(phrase.replace("khan","shayan"))#it will replace
+
+
+#Working with Numbers in Python
+
+print(2)
+print(2.33343)
+print(4+9)
+print(3*5+4.4)
+print(3*(54/4))
+
+
+my_num = 5
+print(my_num)
+print(str(my_num))
+print(str(my_num)+ " its my lucky number")
+print(pow(3,4))# it mean power
+print(max(4,6))
+print(round(3.9))
+
+from math import *
+print(floor((3.7)))
+print(ceil(3.7))
+print(sqrt(25))
+
